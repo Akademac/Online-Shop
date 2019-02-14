0d8a18ba20c60d52b25fdad4fe509ba3a8e25729
Goran Djuric <gdjuric1989@gmail.com>
2019-02-14 20:08:55 +0100

    jela commiting

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 window.addEventListener('load', () => {
+	let about_us_video = document.querySelector('#about_us_video');
+	let play_about_us_video = document.querySelector('.fa-play-circle');
+
+	play_about_us_video.addEventListener('click', () => {
+		about_us_video.play();
+		play_about_us_video.style.display = 'none';		
+	});
+
+	about_us_video.onended = function() {
+		play_about_us_video.style.display = 'block';
+	};
 });
