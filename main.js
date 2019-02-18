efeb9a4b6d192b12f37b918184dbdf74551fc4eb
Goran Djuric <gdjuric1989@gmail.com>
2019-02-18 20:11:03 +0100

    adding JSON #1

--- main.js
+++ main.js
@@ -10,4 +10,25 @@ window.addEventListener('load', () => {
 	about_us_video.onended = function() {
 		play_about_us_video.style.display = 'block';
 	};
+
+	let nav_options = document.querySelectorAll('.nav_options');
+
+	let nav_options_div = document.querySelector('#nav_hover_options');
+
+	nav_options.forEach(opt => {
+		opt.addEventListener('mouseover', () => {
+			nav_options_div.style.display = 'grid';
+		});
+		opt.addEventListener('mouseout', () => {
+			nav_options_div.style.display = 'none';
+		});
+	});
+
+	nav_options_div.addEventListener('mouseover', () => {
+		nav_options_div.style.display = 'grid';
+		});
+	nav_options_div.addEventListener('mouseout', () => {
+		nav_options_div.style.display = 'none';
+		});
+
 });
