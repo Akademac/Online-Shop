26775d3ad00e9389a19fb60df58600a572d0e29a
Djuki <gdjuric1989@gmail.com>
2021-04-05 00:37:05 +0200

    some changes 2021

--- main.js
+++ main.js
@@ -20,25 +20,25 @@ window.addEventListener('load', () => {
 
 //Main Nav Div	
 
-	let nav_options = document.querySelectorAll('.nav_options');
+	// let nav_options = document.querySelectorAll('.nav_options');
 
-	let nav_options_div = document.querySelector('#nav_hover_options');
+	// let nav_options_div = document.querySelector('#nav_hover_options');
 
-	nav_options.forEach(opt => {
-		opt.addEventListener('mouseover', () => {
-			nav_options_div.style.display = 'grid';
-		});
-		opt.addEventListener('mouseout', () => {
-			nav_options_div.style.display = 'none';
-		});
-	});
+	// nav_options.forEach(opt => {
+	// 	opt.addEventListener('mouseover', () => {
+	// 		nav_options_div.style.display = 'grid';
+	// 	});
+	// 	opt.addEventListener('mouseout', () => {
+	// 		nav_options_div.style.display = 'none';
+	// 	});
+	// });
 
-	nav_options_div.addEventListener('mouseover', () => {
-		nav_options_div.style.display = 'grid';
-		});
-	nav_options_div.addEventListener('mouseout', () => {
-		nav_options_div.style.display = 'none';
-		}); 
+	// nav_options_div.addEventListener('mouseover', () => {
+	// 	nav_options_div.style.display = 'grid';
+	// 	});
+	// nav_options_div.addEventListener('mouseout', () => {
+	// 	nav_options_div.style.display = 'none';
+	// 	}); 
 	
 //Banner
 	
